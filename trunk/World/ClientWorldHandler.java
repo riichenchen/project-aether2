9ef5acfc097ba4751995dd1b16a423652102b297
shiratori.seenbeen@gmail.com <shiratori.seenbeen@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-06 01:13:58 +0000

    -init upload of new proj -seenbeen

---
+++ trunk/World/ClientWorldHandler.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)ClientWorldHandler.java
+ *
+ *
+ * @author 
+ * @version 1.00 2013/5/5
+ */
+
+import java.util.*;
+
+public class ClientWorldHandler {
+
+    public static void main(String[] args) throws Exception{
+    	Scanner kb = new Scanner(System.in);
+    }
+    
+    
+}
