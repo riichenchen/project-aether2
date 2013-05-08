468c7d4ce09bd647bd15c8c7e32f2b37fc0d9e9c
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-08 14:51:02 +0000

    Added Templates

---
+++ trunk/src/GameSource/Game/GamePanel.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.*;
+
+public class GamePanel extends JApplet {
+	public GamePanel() {
+		super();
+		setSize(800, 600);
+		setVisible(true);
+	}
+
+}
