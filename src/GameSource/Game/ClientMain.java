574d2b5478ca63c89792b46b57c05d4ed48587be
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-09 03:15:04 +0000

    Added LoginFrame (Can replace with GUI later)

--- src/GameSource/Game/ClientMain.java
+++ src/GameSource/Game/ClientMain.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
 
 public class ClientMain {
-	
-	private static void startGame() {
-		JFrame f = new JFrame("GAME_NAME");
-		GamePanel gp = new GamePanel();
-		f.getContentPane().add(gp);
+        
+        public static JFrame f;
+    
+	private static void login() {
+		f = new JFrame("GAME_NAME");
+                LoginFrame lf = new LoginFrame();
+		f.getContentPane().add(lf);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(800, 600);
+		f.setSize(LoginFrame.X, LoginFrame.Y);
 		f.setVisible(true);
 	}
+        
+        public static void startGame() {
+            System.out.println("startGame");
+        }
 	
 	public static void main(String [] args) {
-		startGame();
+		login();
 	}
 }
