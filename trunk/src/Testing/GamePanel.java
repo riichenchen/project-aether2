242138922296c7f40d67baec60f58e3e9b25f83c
shiratori.seenbeen@gmail.com <shiratori.seenbeen@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-11 02:24:35 +0000

    -Testing for input controls
    -Press space to remove the control from 1 dude and space again to add it.
         -Full potential of AbstractKeyListener can be seen C:<
    
    -Seenbeen

--- trunk/src/Testing/GamePanel.java
+++ trunk/src/Testing/GamePanel.java
@@ -10,9 +10,7 @@ import GameSource.Assets.TerrainBlocks.Blocks.otherblock.Other_Block;
 import GameSource.game.GameMap;
 import Input.InputManager;
 import Renderer.AetherCam;
-import Renderer.RenderMessage;
 import Renderer.Renderer;
-import Spatial.Spatial;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
@@ -31,8 +29,8 @@ public class GamePanel extends JPanel implements MouseMotionListener,KeyListener
     public boolean ready=false,inputstate = true;
     //private int boxx= 200,boxy=200;
     private Renderer myRender;
-    private Other_Block myBlock;
-    private MyKeyListener mykeycontrol;
+    private Other_Block myBlock,yourBlock;
+    private MyKeyListener mykeycontrol,yourkeycontrol;
     private int ids = 0;
     private AetherCam myCam = new AetherCam(800,600);
     public GamePanel(){ //initialize game variables, assign map to UI
@@ -44,10 +42,14 @@ public class GamePanel extends JPanel implements MouseMotionListener,KeyListener
         //new SpriteSet();
         AssetManager.init();
         myBlock = new Other_Block(300,1,200,ids++);
+        yourBlock = new Other_Block(350,1,200,ids++);
         mykeycontrol = new MyKeyListener();
+        yourkeycontrol = new MyKeyListener();
         myBlock.addControl(mykeycontrol);
+        yourBlock.addControl(yourkeycontrol);
         myRender = new Renderer(new GameMap("Mymap",1));
         myRender.addSpatial(myBlock);
+        myRender.addSpatial(yourBlock);
         myRender.setCam(myCam);
         Random myrand = new Random();
         for (int i = 0; i < 5000; i++){
@@ -65,11 +67,13 @@ public class GamePanel extends JPanel implements MouseMotionListener,KeyListener
         ready = true;
     }
     public void update(){
-        myBlock.update();
+        updateSpatials();
         myRender.update();
     }
-    public void updateSpatial(Spatial spat){
-        
+    
+    private void updateSpatials(){
+        myBlock.update();
+        yourBlock.update();
     }
     public void move(){
         if (keys[KeyEvent.VK_D]){
