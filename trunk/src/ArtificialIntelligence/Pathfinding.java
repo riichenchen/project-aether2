8b0edc327aad8d90f4b2712deb0f9c5aa91a749f
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-22 14:56:57 +0000

    Started A* for path finding.

---
+++ trunk/src/ArtificialIntelligence/Pathfinding.java
@@ -0,0 +1,22 @@
+
+
+public class Pathfinding {
+	private char [][] grid;
+	private int [][] dist, X_MAX, Y_MAX, [] dx = {1, 0, -1, 0}, [] dy = {0, 1, 0, -1};
+	
+	
+	public Pathfinding(char [][] _grid) {
+		grid = _grid;
+		X_MAX = grid.length;
+		Y_MAX = grid[0].length;
+		init();
+	}	
+		
+	private init() {
+		for (int i = 0; i < X_MAX; i++)
+			for (int j = 0; j < Y_MAX; j++) {
+				if ()
+			}
+	}
+	
+}
