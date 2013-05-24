74a7eff748849f542345e19f61e9d64614b2c5b5
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-24 14:28:31 +0000

    Pathfinding works given a character array of valid / invalid positions MobCounter determines the number of monsters in a given area (x1, y1, x2, y2) in log square n time

---
+++ trunk/src/ArtificialIntelligence/MobCounter.java
@@ -0,0 +1,68 @@
+/*public class MobCounter {
+	
+	public int X_MAX, Y_MAX;
+	private int [][] tree;
+	
+	
+	public MobCounter(char [][] grid) {
+		
+		X_MAX = grid.length;
+		Y_MAX = grid[0].length;
+		
+		X_MAX++; Y_MAX++;
+		tree = new int[X_MAX + 1][Y_MAX + 1];
+		
+		for (int i = 1; i < X_MAX; i++)
+			for (int j = 1; j < Y_MAX; j++)
+				if (grid[i - 1][j - 1] == 1)
+					update(i, j, 1);
+	}
+	
+	public void removeMob(int x, int y) {
+		update(x + 1, y + 1, -1);
+	}
+	
+	public void addMob(int x, int y) {
+		update(x + 1, y + 1, 1);
+	}
+		
+	int getNumMobs(int x1, int y1, int x2, int y2) {
+		return query(x2 + 1, y2 + 1) - query(x1 , y2 + 1) - query(x2 + 1, y1) + query(x1, y1);
+	}
+	
+	private void update(int x, int y, int d) {
+		while (x < X_MAX + 1) {
+			int y1 = y;
+			while (y1 < Y_MAX + 1) {
+				tree[x][y1] += d;
+				y1 += (y1 & -y1);
+			}
+			x += (x & -x);
+		}
+	}
+	
+	private int query(int x, int y) {
+		int sum = 0;
+		while (x > 0) {
+			int y1 = y;
+			while (y1 > 0) {
+				sum += tree[x][y1];
+				y1 -= (y1 & -y1); 
+			}
+			x -= (x & -x);
+		}
+		return sum;
+	}
+	
+	public static void main(String [] args) {
+		char [][] testGrid = { {1, 1, 1, 1},
+								{1, 0, 1, 1},
+								{1, 1, 0, 1}
+		};
+		MobCounter testCounter = new MobCounter(testGrid);
+		System.out.println(testCounter.getNumMobs(1, 1, 2, 2));
+		
+	}
+	
+	
+}*/
