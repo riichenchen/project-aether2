0d6bcff26f935cb061c71f715b4ec8e26e4a2a6d
shiratori.seenbeen@gmail.com <shiratori.seenbeen@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-05-28 00:26:17 +0000

    -Eventkeys added along with simple keydowns
    -Seenbeen

--- src/Input/InputManager.java
+++ src/Input/InputManager.java
@@ -14,19 +14,30 @@ import java.util.HashMap;
 public class InputManager {
     private static HashMap<Integer,AbstractKeyListener> listeners = new HashMap<>();
     private static boolean[] keys = new boolean[KeyEvent.KEY_LAST+1];
+//    private static boolean[] eventKeys = new boolean[KeyEvent.KEY_LAST+1];
+    private static boolean[] pressedKeys = new boolean[KeyEvent.KEY_LAST+1];
+    
     private static int IDs = 0;//used to track number of listeners
     
     public static void keyDown(int keyId){
         keys[keyId] = true;
+//        if (!pressedKeys[keyId]){
+//        eventKeys[keyId] = true;
+        pressedKeys[keyId] = true;
+            //System.out.println("hai");
+//        } 
     }
+    
     public static void keyUp(int keyId){
         keys[keyId] = false;
+        pressedKeys[keyId] = false;
+//        System.out.println("AHHHI");
     }
     
     //Gives a pointer to keys to each listener
     public static void addKeyListener(AbstractKeyListener listener){
         listeners.put(IDs,listener);
-        listener.bindTo(keys,IDs++);
+        listener.bindTo(keys,pressedKeys,IDs++);
     }
     
 }
