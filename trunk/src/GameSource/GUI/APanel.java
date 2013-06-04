4b0cb181492cfb0b28390666c63d3b2e7d055f12
lonemoonstar@gmail.com <lonemoonstar@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-06-04 12:23:15 +0000

    USB broke >.> Added APanel class for easier switching between panes in things like the inventory or equipment -- only one I could finish in the time I had.

---
+++ trunk/src/GameSource/GUI/APanel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameSource.GUI;
+
+import java.awt.Graphics;
+import java.util.Map;
+
+/**
+ *
+ * @author Joy
+ */
+public class APanel extends AComponent{
+    private Map<String, AComponent> panes;
+    private String activePane;
+    
+    class APanelButton extends AButton{
+        private APanel panel;
+        public APanelButton(String name, int mtype, String mcontent, APanel p){
+            super(name,mtype, mcontent);
+            panel=p;
+        }
+        public void call(){
+            panel.setActivePane(message.content());
+        }
+    }
+    public void setActivePane(String name){
+        activePane=name;
+    }
+    public void add(AComponent a){
+        panes.put(a.getName(),a);
+    }
+    public void call(){
+        panes.get(activePane).call();
+    }
+    public void draw(Graphics g){
+        panes.get(activePane).draw(g);
+    }
+    public void update(){
+        panes.get(activePane).update();
+    }
+}
