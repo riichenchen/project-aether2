d2cbc256b99331f7a6af7bd8fbe0dd441749dc7d
shiratori.seenbeen@gmail.com <shiratori.seenbeen@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-06-09 12:27:03 +0000

    -Exp bug fixed for leveling
    -Seenbeen

--- trunk/src/GameSource/User/LevelManager.java
+++ trunk/src/GameSource/User/LevelManager.java
@@ -9,22 +9,7 @@ package GameSource.User;
  * @author Shiyang
  */
 public class LevelManager {
-    private static int requiredExp(int level){
+    public static int requiredExp(int level){
         return (int)(10*(level+Math.pow(1.175,level)));
-    }
-    public static void addExp(int exp){
-        do {
-            int currentExp = CharacterHandler.getStat("exp");
-            int currentLevel = CharacterHandler.getStat("level");
-            int required = requiredExp(currentLevel);
-            if (currentExp+exp > required){
-                int dif = currentExp+exp-required;
-                CharacterHandler.addStat("level",1);
-                CharacterHandler.addStat("exp",dif-currentExp);
-                System.out.println("level up! to level "+CharacterHandler.getStat("level"));
-            } else {
-                break; // continuously level (incase overleveling occurs, which is really unlikely
-            }
-        } while (true);
     }
 }
