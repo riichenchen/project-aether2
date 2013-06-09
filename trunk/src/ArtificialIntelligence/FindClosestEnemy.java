c6a5a8af69d4ae91bc2135a44437612e5cdabc51
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-06-09 21:27:44 +0000

    Re-did FindClosestEnemy with AbstractMob classification

--- trunk/src/ArtificialIntelligence/FindClosestEnemy.java
+++ trunk/src/ArtificialIntelligence/FindClosestEnemy.java
@@ -1,8 +1,40 @@
+import GameSource.Assets.MobData.AbstractMob;
+import Spatial.Spatial;
 import java.util.*;
 
 public class FindClosestEnemy {
-	public FindClosestEnemy (ArrayList players, ArrayList mobs) {
-		
-		
+        
+        protected int bestIdx;
+        protected double best, curX, curY;
+        protected ArrayList spats;
+        protected final double EPS = 1e-10;
+    
+        private double calcDist(Spatial spat) {
+            double x = spat.getX();
+            double y = spat.getY();
+            return Math.sqrt((x - curX) * (x - curX) + (y - curY) * (y - curY));
+        }
+        
+	public FindClosestEnemy (double _curX, double _curY, ArrayList _spats) {
+            bestIdx = -1;
+            curX = _curX;
+            curY = _curY;
+            spats = _spats;
+            best = 1 << 30;
+            
+            for (int i = 0; i < spats.size(); i++) {
+                Spatial curSpat = (Spatial) spats.get(i);
+		if (!(curSpat instanceof AbstractMob)) {
+                    double curDist = calcDist(curSpat);
+                    if (curDist < best) {
+                        best = curDist;
+                        bestIdx = i;
+                    }
+                }
+            }
 	}
+        
+        public Spatial getTarget() {
+            return (Spatial)spats.get(bestIdx);
+        }
 }
