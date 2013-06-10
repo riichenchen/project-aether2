3d0ba5a289535518615c28f331670c1483d4034c
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-06-10 14:57:50 +0000

    Added stuff to CowAIControl.java

--- trunk/src/GameSource/Controls/CowAIControl.java
+++ trunk/src/GameSource/Controls/CowAIControl.java
@@ -4,16 +4,21 @@
  */
 package GameSource.Controls;
 
+import ArtificialIntelligence.Pathfinding;
+import ArtificialIntelligence.FindClosestEnemy;
 import ArtificialIntelligence.AIControl;
+import ArtificialIntelligence.Node;
 import GameSource.game.GameMap;
+import Spatial.Spatial;
 import Testing.CowAICalculation;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  *
  * @author Shiyang
  */
-public class CowAIControl extends AIControl{
+public class CowAIControl extends AIControl {
 
     protected GameMap curMap;
     
@@ -24,14 +29,27 @@ public class CowAIControl extends AIControl{
 
     @Override
     public void update(Object returnValue) {
+        
         int curX = (int)boundTo.getX();
         int curY = (int)boundTo.getY();
         
-//        System.out.println("Updating cow "+boundTo.getId());
-//        Random meep = new Random();
-//        int xdist = meep.nextInt(3)-1;
-//        int ydist = meep.nextInt(3)-1;
-//        boundTo.move(xdist*5, 0, ydist*5);
+        FindClosestEnemy fce = new FindClosestEnemy(curX, curY, curMap.getSpatials());
+        Spatial target = fce.getTarget();
+        
+        Pathfinding pf = new Pathfinding(curMap.getCharMap(), curX, curY, (int)target.getX(), (int)target.getY());
+        ArrayList myPath = pf.getPath();
+        
+        if (myPath.isEmpty()) {
+            Random rnd = new Random();
+            int xdist = rnd.nextInt(5)-1;
+            int ydist = rnd.nextInt(5)-1;
+            boundTo.move(xdist * 5, 0, ydist * 5);
+        } else {
+            int dx = ((Node)(myPath.get(0))).getX() - curX;
+            int dy = ((Node)(myPath.get(0))).getY() - curY;
+
+            boundTo.move(dx * 5, 0, dy * 5);
+        }
     }
     
 }
