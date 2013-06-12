4c7997776eeb2c6f5264b1120e11df119b1922b2
aekong1@gmail.com <aekong1@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-06-12 04:41:26 +0000

    Revised Control to use PhysicsSpace *DummySpatial not working

--- trunk/src/GameSource/Controls/CowAIControl.java
+++ trunk/src/GameSource/Controls/CowAIControl.java
@@ -1,23 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package GameSource.Controls;
 
 import ArtificialIntelligence.Pathfinding;
 import ArtificialIntelligence.FindClosestEnemy;
 import ArtificialIntelligence.AIControl;
+import ArtificialIntelligence.GrabSpatial;
+import ArtificialIntelligence.MapParser;
 import ArtificialIntelligence.Node;
+import GameSource.Assets.MobData.AbstractMob;
 import GameSource.game.GameMap;
 import Spatial.Spatial;
 import Testing.CowAICalculation;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- *
- * @author Shiyang
- */
 public class CowAIControl extends AIControl {
 
     protected GameMap curMap;
@@ -26,7 +21,10 @@ public class CowAIControl extends AIControl {
     protected FindClosestEnemy fce;
     protected int curX, curY;
     protected Spatial target;
+    protected MapParser mp;
     protected boolean isLoaded;
+    protected GrabSpatial gs;
+    protected Spatial [][] inRange;
     
     public CowAIControl(GameMap map) {
         super(new CowAICalculation());
@@ -38,40 +36,55 @@ public class CowAIControl extends AIControl {
 
     @Override
     public void update(Object returnValue) {
-//        if (boundTo != null && !isLoaded)
-//            init();
-//        
-//        if (myPath == null)
-//            return;
-//        
-//        if (myPath.isEmpty()) {
-//            Random rnd = new Random();
-//            int xdist = rnd.nextInt(5) - 1;
-//            int ydist = rnd.nextInt(5) - 1;
-//            boundTo.move(xdist, 0, ydist);
-//        } else {
-//            int dx = ((Node)(myPath.get(0))).getX() - curX;
-//            int dy = ((Node)(myPath.get(0))).getY() - curY;
-//         
-//            boundTo.move(dx, 0, dy);
-//       }
+        if (boundTo != null && !isLoaded)
+            init();
+        
+        if (myPath == null)
+            return;
+        
+        if (myPath.isEmpty()) {
+            Random rnd = new Random();
+            int xdist = rnd.nextInt(5) - 1;
+            int ydist = rnd.nextInt(5) - 1;
+            boundTo.move(xdist, 0, ydist);
+        } else {
+            int dx = ((Node)(myPath.get(0))).getX() - curX;
+            int dy = ((Node)(myPath.get(0))).getY() - curY;
+         
+            boundTo.move(dx, 0, dy);
+      }
     }
     
     public void init() {
         curX = (int)boundTo.getX();
         curY = (int)boundTo.getZ();
         
-        if (curMap == null) {
-            System.out.println("Are you serious!");
-        }
+        gs = new GrabSpatial(curMap, curX, curY);
+        
+        inRange = gs.getSpatials();
+        
+        mp = new MapParser(inRange);
+        
 
-        fce = new FindClosestEnemy(curX, curY, curMap.getSpatials());
+        fce = new FindClosestEnemy(curX, curY, inRange);
         target = fce.getTarget();
 
-        pf = new Pathfinding(curMap.getCharMap(), curX, curY, (int)target.getX(), (int)target.getY());
+        pf = new Pathfinding(mp.getCharMap(), curX - mp.getxOffset(), curY - mp.getyOffset(), (int)target.getX() - mp.getxOffset(), (int)target.getY() - mp.getyOffset());
         myPath = pf.getPath();
         
         isLoaded = true;
+    }
+    
+    public int countSpatials() {
+        ArrayList curSpatials = curMap.getSpatials();
+        int size = curSpatials.size();
+        int numMob = 0;
+        for (int i = 0; i < size; i++) {
+            if (curSpatials.get(i) instanceof AbstractMob) {
+                numMob++;
+            }
+        }
+        return numMob;
     }
     
 }
