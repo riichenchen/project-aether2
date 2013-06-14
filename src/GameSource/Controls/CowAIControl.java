7ee9234cb9f233354d7c4ae1a6b64667e31a1348
shiratori.seenbeen@gmail.com <shiratori.seenbeen@gmail.com@5447926f-8fe9-2a66-d700-f3636ff53393>
2013-06-14 01:25:15 +0000

    works now lol

--- src/GameSource/Controls/CowAIControl.java
+++ src/GameSource/Controls/CowAIControl.java
@@ -14,6 +14,7 @@ public class CowAIControl extends AIControl {
     private final double DEFAULT_AGGRO_RADIUS = 128;
     private final double DEFAULT_SHORT_ATTACK_RANGE = 5;
     private final double DEFAULT_LONG_ATTACK_RANGE = 15;
+    private final int DEFAULT_DELAY = 8;
     
     private final int SHORT_ATTACK = 0, LONG_ATTACK = 1;
     
@@ -25,40 +26,50 @@ public class CowAIControl extends AIControl {
     protected double aggroRadius;
     protected double dist;
     protected Random rnd;
-    int f = 0;
+    protected int slow;
+    protected int delay;
+    
     public CowAIControl(GameMap map) {
         super(new CowAICalculation());
         curMap = map;
         aggroRadius = DEFAULT_AGGRO_RADIUS;
         shortAttackRange = DEFAULT_SHORT_ATTACK_RANGE;
         longAttackRange = DEFAULT_LONG_ATTACK_RANGE;
-        rnd = new Random();
-        pf = new Pathfinding(map.getCharMap(), -1, -1, -1, -1);
+        delay = DEFAULT_DELAY;
+        init();
     }
     
-    public CowAIControl(GameMap map, double _aggroRadius, double _shortAttackRange, double _longAttackRange) {
+    public CowAIControl(GameMap map, double _aggroRadius, double _shortAttackRange, double _longAttackRange, int _delay) {
         super(new CowAICalculation());
         curMap = map;
         aggroRadius = _aggroRadius;
         shortAttackRange = _shortAttackRange;
         longAttackRange = _longAttackRange;
-        
+        delay = _delay;
+        init();
     }
-
+    private void init() {
+        rnd = new Random();
+        pf = new Pathfinding(curMap.getCharMap(), -1, -1, -1, -1);
+        slow = 0;     
+    }
     @Override
     public void update(Object returnValue) {
-        getLocations();
-        int attackType = rnd.nextInt() % 2;
-        if (attackType == SHORT_ATTACK) {
-            if (dist <= shortAttackRange) {
-                //boundTo.attack(CharacterHandler.getPlayer(), SHORT_ATTACK);
-            } else
-                move();
-        } else {
-            if (dist <= longAttackRange) {
-                //boundTo.attack(CharacterHandler.getPlayer(), LONG_ATTACK);
-            } else
-                move();
+        if (slow++ == delay) {
+            getLocations();
+            int attackType = rnd.nextInt() % 2;
+            if (attackType == SHORT_ATTACK) {
+                if (dist <= shortAttackRange) {
+                    //boundTo.attack(CharacterHandler.getPlayer(), SHORT_ATTACK);
+                } else
+                    move();
+            } else {
+                if (dist <= longAttackRange) {
+                    //boundTo.attack(CharacterHandler.getPlayer(), LONG_ATTACK);
+                } else
+                    move();
+            }
+            slow = 0;
         }
     }
     
@@ -71,8 +82,10 @@ public class CowAIControl extends AIControl {
     }
     
     private void move() {
-        //pf.updateLocations(curX, curY, tarX, tarY);
-        boundTo.move(tarX - curX, 0, tarY - curY);
+        //System.out.printf("curx %d cury %d tarx %d tary %d\n", curX, curY, tarX, tarY);
+        pf.updateLocations(curX, curY, tarX, tarY);
+        //System.out.printf("%d %d\n", pf.getToX(), pf.getToY());
+        boundTo.move(pf.getToX() - curX, 0, pf.getToY() - curY);
         //System.out.printf("Moved x: %d y: %d(%d - %d)\n", tarX - curX, tarY - curY, tarY, curY);
         //System.out.printf("Current: %d %d %d\nTarget: %d %d %d\n", (int)boundTo.getX(), (int)boundTo.getY(), (int)boundTo.getZ(), (int)CharacterHandler.getPlayer().getX(), (int)CharacterHandler.getPlayer().getY(), (int)CharacterHandler.getPlayer().getZ());
     }
